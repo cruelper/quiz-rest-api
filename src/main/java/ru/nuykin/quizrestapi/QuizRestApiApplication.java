855266366c8b5d8b6d1642efe04d2739d19d1585
cruelper <cruelper@yandex.ru>
2023-05-15 18:57:55 +0400

    init quiz rest api project

---
+++ src/main/java/ru/nuykin/quizrestapi/QuizRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.nuykin.quizrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nuykin/quizrestapi/QuizRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nuykin.quizrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
